303adeea0bc2ed007b1e45a4eabf7d94052e7d0d
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-04-15 00:08:43 -0400

    feat: add script to generate and save simulation state screenshots

--- scripts/generate_scr.py
+++ scripts/generate_scr.py
@@ -9,13 +9,25 @@ screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
 from sim import Simulation
-from render import render
+from render import render_overworld
 
 sim = Simulation()
-for i in range(3600):
+# Run for a while to get some activity
+for i in range(1200):
     sim.update()
 
-# Force a render
-render(screen, sim, clock)
-pygame.image.save(screen, "screenshots\screenshot_phase2c.png")
-print("Saved screenshot_phase2c.png")
+# Create surfaces for split-view
+overworld_surface = pygame.Surface((800, 400))
+nest_surface = pygame.Surface((800, 200))
+
+# Render both
+render_overworld(overworld_surface, sim, clock)
+sim.nest_renderer.render_nest(nest_surface, sim.nest, sim.queen, sim.ants_in_nest, sim.food_storage)
+
+# Blit to screen
+screen.blit(overworld_surface, (0, 0))
+screen.blit(nest_surface, (0, 400))
+pygame.draw.line(screen, (100, 100, 100), (0, 400), (800, 400), 2)
+
+pygame.image.save(screen, "screenshots/screenshot_phase3.png")
+print("Saved screenshot_phase3.png")
